cb50098b6eb99d8a870afbac78c0f1577a9fa2be
1iveowl <liveowl@outlook.com>
2016-11-22 20:22:49 +0100

    .NET Core support - bumpd tp v.3.5.0

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # SSDP UPnP v2.0 Library
 
 
-[![NuGet Badge](https://buildstats.info/nuget/SSDP.UPnP.PCL)](https://www.nuget.org/packages/RSSDP/)
+[![NuGet Badge](https://buildstats.info/nuget/SSDP.UPnP.PCL)](https://github.com/1iveowl/SSDP.UPnP.PCL)
 
 [![.NET Standard](http://img.shields.io/badge/.NET_Standard-v1.2-green.svg)](https://docs.microsoft.com/da-dk/dotnet/articles/standard/library) [![System.Reactive](http://img.shields.io/badge/Rx-v3.1.0-ff69b4.svg)](http://reactivex.io/) 
 
---
+++ src/test/SSDP.Console.NETcore.Test/Model/UserAgent.cs
@@ -0,0 +1,16 @@
+﻿using ISSDP.UPnP.PCL.Interfaces.Model;
+
+namespace SSDP.Console.NETcore.Test.Model
+{
+    internal class UserAgent : IUserAgent
+    {
+        public string FullString { get; internal set; }
+        public string OperatingSystem { get; internal set; }
+        public string OperatingSystemVersion { get; internal set; }
+        public string ProductName { get; internal set; }
+        public string ProductVersion { get; internal set; }
+        public string UpnpMajorVersion { get; internal set; }
+        public string UpnpMinorVersion { get; internal set; }
+        public bool IsUpnp2 { get; internal set; }
+    }
+}
