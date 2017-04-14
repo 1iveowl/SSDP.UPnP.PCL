1e8cb54fe7380c7605ed64c209ce9f0a94bb7b6f
1iveowl <liveowl@outlook.com>
2017-04-14 19:03:52 +0200

    v3.7.2

--- src/interfaces/Properties/AssemblyInfo.cs
+++ src/interfaces/Properties/AssemblyInfo.cs
@@ -27,6 +27,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.7.0.0")]
-[assembly: AssemblyFileVersion("3.7.0.0")]
+[assembly: AssemblyVersion("3.7.2.0")]
+[assembly: AssemblyFileVersion("3.7.2.0")]
 
